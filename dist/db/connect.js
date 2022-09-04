c82ba2933d3862e31712c0583af033c50b62c065
Guilherme Augusto Deitos Alves <65348931+GuilhermeDeitos@users.noreply.github.com>
2022-09-04 10:52:54 -0300

    To-Do Api finalizada

---
+++ dist/db/connect.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MongooseConnect = void 0;
+const mongoose_1 = require("mongoose");
+exports.MongooseConnect = mongoose_1.default.connect('mongodb://localhost:27017/Api_Tasks');
+//# sourceMappingURL=connect.js.map
